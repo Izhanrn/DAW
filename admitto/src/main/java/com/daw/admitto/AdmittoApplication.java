ad5e77c0fb84fc3e29e831bcedcb5269837df2ee
Izhanrn <91908276+Izhanrn@users.noreply.github.com>
2026-04-01 11:36:49 +0200

    Add files via upload

---
+++ admitto/src/main/java/com/daw/admitto/AdmittoApplication.java
@@ -0,0 +1,13 @@
+package com.daw.admitto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmittoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmittoApplication.class, args);
+	}
+
+}
---
+++ admitto/src/test/java/com/daw/admitto/AdmittoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.admitto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmittoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
